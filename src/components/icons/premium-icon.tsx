1dc359f93460409651c7d935c8c0fee480eb9ab3
Jody Munnik <JodyMunnik@gmail.com>
2025-10-15 16:36:51 +0000

    make red dot smaller and remove toolbar line completely  (_for element b

--- src/components/icons/premium-icon.tsx
+++ src/components/icons/premium-icon.tsx
@@ -5,9 +5,9 @@ export function PremiumIcon(props: LucideProps) {
   return (
     <div className="relative">
       <Crown {...props} />
-      <span className="absolute -top-0.5 -right-0.5 flex h-3 w-3">
+      <span className="absolute -top-0.5 -right-0.5 flex h-2 w-2">
         <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-        <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+        <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
       </span>
     </div>
   );
